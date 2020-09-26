c351e036b4b0c968e8afe1b092416056d2a7db03
wgzesg <wgzesg@gmail.com>
2020-09-26 17:26:10 +0800

    put back all functions that I commented out

--- routes/init.js
+++ routes/init.js
@@ -15,9 +15,7 @@ const salt  = bcrypt.genSaltSync(round);
 
 function initRouter(app) {
 	/* GET */
-	app.get('/'      , function(req, res) {
-		res.send('Hello inside get/!');
-	});
+	app.get('/'      , index);
 	app.get('/search', search);
 	
 	/* PROTECTED GET */
@@ -73,101 +71,94 @@ function msg(req, fld, pass, fail) {
 
 // GET
 function index(req, res, next) {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
-	// var ctx = 0, idx = 0, tbl, total;
-	// if(Object.keys(req.query).length > 0 && req.query.p) {
-	// 	idx = req.query.p-1;
-	// }
-	// pool.query(sql_query.query.page_lims, [idx*10], (err, data) => {
-	// 	if(err || !data.rows || data.rows.length == 0) {
-	// 		tbl = [];
-	// 	} else {
-	// 		tbl = data.rows;
-	// 	}
-	// 	pool.query(sql_query.query.ctx_games, (err, data) => {
-	// 		if(err || !data.rows || data.rows.length == 0) {
-	// 			ctx = 0;
-	// 		} else {
-	// 			ctx = data.rows[0].count;
-	// 		}
-	// 		total = ctx%10 == 0 ? ctx/10 : (ctx - (ctx%10))/10 + 1;
-	// 		console.log(idx*10, idx*10+10, total);
-	// 		if(!req.isAuthenticated()) {
-	// 			res.render('index', { page: '', auth: false, tbl: tbl, ctx: ctx, p: idx+1, t: total });
-	// 		} else {
-	// 			basic(req, res, 'index', { page: '', auth: true, tbl: tbl, ctx: ctx, p: idx+1, t: total });
-	// 		}
-	// 	});
-	// });
+	var ctx = 0, idx = 0, tbl, total;
+	if(Object.keys(req.query).length > 0 && req.query.p) {
+		idx = req.query.p-1;
+	}
+	pool.query(sql_query.query.page_lims, [idx*10], (err, data) => {
+		if(err || !data.rows || data.rows.length == 0) {
+			tbl = [];
+		} else {
+			tbl = data.rows;
+		}
+		pool.query(sql_query.query.ctx_games, (err, data) => {
+			if(err || !data.rows || data.rows.length == 0) {
+				ctx = 0;
+			} else {
+				ctx = data.rows[0].count;
+			}
+			total = ctx%10 == 0 ? ctx/10 : (ctx - (ctx%10))/10 + 1;
+			console.log(idx*10, idx*10+10, total);
+			if(!req.isAuthenticated()) {
+				res.render('index', { page: '', auth: false, tbl: tbl, ctx: ctx, p: idx+1, t: total });
+			} else {
+				basic(req, res, 'index', { page: '', auth: true, tbl: tbl, ctx: ctx, p: idx+1, t: total });
+			}
+		});
+	});
 }
 function search(req, res, next) {
-	res.send("inside search");
-	// var ctx  = 0, avg = 0, tbl;
-	// var game = "%" + req.query.gamename.toLowerCase() + "%";
-	// pool.query(sql_query.query.search_game, [game], (err, data) => {
-	// 	if(err || !data.rows || data.rows.length == 0) {
-	// 		ctx = 0;
-	// 		tbl = [];
-	// 	} else {
-	// 		ctx = data.rows.length;
-	// 		tbl = data.rows;
-	// 	}
-	// 	if(!req.isAuthenticated()) {
-	// 		res.render('search', { page: 'search', auth: false, tbl: tbl, ctx: ctx });
-	// 	} else {
-	// 		basic(req, res, 'search', { page: 'search', auth: true, tbl: tbl, ctx: ctx });
-	// 	}
-	// });
+	var ctx  = 0, avg = 0, tbl;
+	var game = "%" + req.query.gamename.toLowerCase() + "%";
+	pool.query(sql_query.query.search_game, [game], (err, data) => {
+		if(err || !data.rows || data.rows.length == 0) {
+			ctx = 0;
+			tbl = [];
+		} else {
+			ctx = data.rows.length;
+			tbl = data.rows;
+		}
+		if(!req.isAuthenticated()) {
+			res.render('search', { page: 'search', auth: false, tbl: tbl, ctx: ctx });
+		} else {
+			basic(req, res, 'search', { page: 'search', auth: true, tbl: tbl, ctx: ctx });
+		}
+	});
 }
 function dashboard(req, res, next) {
-	res.send("inside dashboard");
-	//basic(req, res, 'dashboard', { info_msg: msg(req, 'info', 'Information updated successfully', 'Error in updating information'), pass_msg: msg(req, 'pass', 'Password updated successfully', 'Error in updating password'), auth: true });
+	basic(req, res, 'dashboard', { info_msg: msg(req, 'info', 'Information updated successfully', 'Error in updating information'), pass_msg: msg(req, 'pass', 'Password updated successfully', 'Error in updating password'), auth: true });
 }
 function games(req, res, next) {
-	res.send("inside game");
-	// var ctx = 0, avg = 0, tbl;
-	// pool.query(sql_query.query.avg_rating, [req.user.username], (err, data) => {
-	// 	if(err || !data.rows || data.rows.length == 0) {
-	// 		avg = 0;
-	// 	} else {
-	// 		avg = data.rows[0].avg;
-	// 	}
-	// 	pool.query(sql_query.query.all_games, [req.user.username], (err, data) => {
-	// 		if(err || !data.rows || data.rows.length == 0) {
-	// 			ctx = 0;
-	// 			tbl = [];
-	// 		} else {
-	// 			ctx = data.rows.length;
-	// 			tbl = data.rows;
-	// 		}
-	// 		basic(req, res, 'games', { ctx: ctx, avg: avg, tbl: tbl, game_msg: msg(req, 'add', 'Game added successfully', 'Game does not exist'), auth: true });
-	// 	});
-	// });
+	var ctx = 0, avg = 0, tbl;
+	pool.query(sql_query.query.avg_rating, [req.user.username], (err, data) => {
+		if(err || !data.rows || data.rows.length == 0) {
+			avg = 0;
+		} else {
+			avg = data.rows[0].avg;
+		}
+		pool.query(sql_query.query.all_games, [req.user.username], (err, data) => {
+			if(err || !data.rows || data.rows.length == 0) {
+				ctx = 0;
+				tbl = [];
+			} else {
+				ctx = data.rows.length;
+				tbl = data.rows;
+			}
+			basic(req, res, 'games', { ctx: ctx, avg: avg, tbl: tbl, game_msg: msg(req, 'add', 'Game added successfully', 'Game does not exist'), auth: true });
+		});
+	});
 }
 function plays(req, res, next) {
-	res.send("inside plays");
-	// var win = 0, avg = 0, ctx = 0, tbl;
-	// pool.query(sql_query.query.count_wins, [req.user.username], (err, data) => {
-	// 	if(err || !data.rows || data.rows.length == 0) {
-	// 		win = 0;
-	// 	} else {
-	// 		win = data.rows[0].count;
-	// 	}
-	// 	pool.query(sql_query.query.all_plays, [req.user.username], (err, data) => {
-	// 		if(err || !data.rows || data.rows.length == 0) {
-	// 			ctx = 0;
-	// 			avg = 0;
-	// 			tbl = [];
-	// 		} else {
-	// 			ctx = data.rows.length;
-	// 			avg = win == 0 ? 0 : win/ctx;
-	// 			tbl = data.rows;
-	// 		}
-	// 		basic(req, res, 'plays', { win: win, ctx: ctx, avg: avg, tbl: tbl, play_msg: msg(req, 'add', 'Play added successfully', 'Invalid parameter in play'), auth: true });
-	// 	});
-	// });
+	var win = 0, avg = 0, ctx = 0, tbl;
+	pool.query(sql_query.query.count_wins, [req.user.username], (err, data) => {
+		if(err || !data.rows || data.rows.length == 0) {
+			win = 0;
+		} else {
+			win = data.rows[0].count;
+		}
+		pool.query(sql_query.query.all_plays, [req.user.username], (err, data) => {
+			if(err || !data.rows || data.rows.length == 0) {
+				ctx = 0;
+				avg = 0;
+				tbl = [];
+			} else {
+				ctx = data.rows.length;
+				avg = win == 0 ? 0 : win/ctx;
+				tbl = data.rows;
+			}
+			basic(req, res, 'plays', { win: win, ctx: ctx, avg: avg, tbl: tbl, play_msg: msg(req, 'add', 'Play added successfully', 'Invalid parameter in play'), auth: true });
+		});
+	});
 }
 
 function register(req, res, next) {
