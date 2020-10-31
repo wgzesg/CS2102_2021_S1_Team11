d3f3a211061eede81b3fdf3ff73ecc88d6b9e5fa
lunzard <60387314+lunzard@users.noreply.github.com>
2020-10-31 19:08:01 +0800

    add trigger for avg rating

--- trigger.sql
+++ trigger.sql
@@ -12,3 +12,34 @@ BEGIN
 	RETURN NEW;
 END;
 $$
+
+
+CREATE OR REPLACE FUNCTION calculate_avg_rating() 
+RETURNS TRIGGER AS $$
+DECLARE
+  ratingsum INTEGER;
+  countsum INTEGER;
+  avgnum INTEGER;
+BEGIN
+  SELECT SUM(R.rating), COUNT(*) INTO ratingsum, countsum
+  FROM reviews R
+  WHERE R.ccontact = NEW.ccontact;
+
+  SELECT CEILING(ratingsum / countsum) INTO avgnum;
+  UPDATE canparttime C SET C.avgrating = avgnum
+    WHERE C.ccontact = NEW.ccontact;
+
+  RETURN NULL;
+
+END;
+$$ LANGUAGE PLPGSQL;
+
+
+
+DROP TRIGGER IF EXISTS
+calculate_avg_rating_trigger ON reviews;
+CREATE TRIGGER calculate_avg_rating_trigger
+  AFTER UPDATE OF rating OR INSERT
+  ON reviews
+  FOR EACH ROW
+  EXECUTE FUNCTION calculate_avg_rating();
