e8610bef238fb0d6a4f5a611cceeced529559340
CFZeon <e0406826@u.nus.edu>
2020-11-02 15:53:27 +0800

    fix label bug

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -198,7 +198,7 @@ def render_caretaker_biddings():
     query = "SELECT * FROM biddings WHERE ccontact = '{}'".format(contact)
     results = db.session.execute(query).fetchall()
     table = biddingCaretakerTable(results)
-    return render_template("caretakerBid.html", table=table, username=current_user.username + " owner")
+    return render_template("caretakerBid.html", table=table, username=current_user.username + " caretaker")
 
 @view.route("/caretaker/biddings/accept", methods=["POST"])
 @roles_required('caretaker')
