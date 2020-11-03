6de6df3d0c2acd594309242a0a64e49b0461578e
CFZeon <e0406826@u.nus.edu>
2020-11-03 21:11:25 +0800

    try fix css again

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -245,7 +245,7 @@ def render_caretaker_available():
     applicationType = "leave"
     ptquery = "SELECT isparttime FROM canparttime WHERE ccontact = '{}'".format(contact)
     isPt = db.session.execute(ptquery).fetchone()
-    if isPt == ("true",):
+    if isPt == (True,):
         applicationType = "availability"
     query = "SELECT * FROM available WHERE ccontact = '{}'".format(contact)
     availables = db.session.execute(query)
