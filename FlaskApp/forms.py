062e7c7bb6d1ff8a288012321e884f7726176647
zsk612 <zhangsk612@gmail.com>
2020-11-03 12:34:27 +0800

    import flash

--- FlaskApp/forms.py
+++ FlaskApp/forms.py
@@ -1,3 +1,4 @@
+from flask import flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField
 from wtforms.fields.html5 import DateField
