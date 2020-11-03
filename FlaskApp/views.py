d6bbb64a8385758e8c2323a5096f27c00086777f
Feng Yuan <gdfy102217@gmail.com>
2020-11-03 12:50:34 +0800

    pagination

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -370,7 +370,6 @@ def render_owner_page(page=1):
     caretaker_page = db.session.execute(pagequery)
     caretable = ownerHomePage(caretaker_page)
 
-    caretable = ownerHomePage(caretakers)
     form = SearchCaretakerForm()
 
     if request.method == 'POST' and form.validate_on_submit():
