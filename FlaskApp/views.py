32e5f8398ffdf3b957302c798f74bb95c78ba935
zsk612 <zhangsk612@gmail.com>
2020-11-07 02:28:58 +0800

    print msg

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -285,6 +285,8 @@ def render_caretaker_biddings_accept():
         query = """SELECT COUNT (*) FROM biddings WHERE '{}' - startday >= 0 AND endday - '{}' >= 0 
             AND ccontact = '{}' AND status = 'success' LIMIT 1""".format(selected, selected, ct)
         count = db.session.execute(query).fetchall()
+        print(parttime, flush=True)
+        print(count, flush=True)
         if parttime[0][1] == (True, ) and int(parttime[0][2]) < (3, ):
             if count[0] > (2,):
                 flag = False
