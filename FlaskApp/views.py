774e70d9e8d6670c28f0d156ffc8811760cd7a4d
Feng Yuan <gdfy102217@gmail.com>
2020-11-06 15:26:22 +0800

    dailyprice

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -179,9 +179,8 @@ def render_admin_update_profile():
 
 @view.route("/admin/dailyprice", methods=["GET", "POST"])
 @roles_required('admin')
-<<<<<<< HEAD
 def render_admin_dailyprice(page=1):
-    countquery = """SELECT COUNT(*) FROM dailyprice"""
+    countquery = """SELECT COUNT(*) FROM dailyprice ORDER BY category, rating"""
     
     count = db.session.execute(countquery).fetchone()
     total = count[0]
@@ -195,23 +194,15 @@ def render_admin_dailyprice(page=1):
     page_offset = (page - 1) * 10
     if total < page * 10:
         page_display = total % 10
-        pagequery = """SELECT * FROM dailyprice
+        pagequery = """SELECT * FROM dailyprice ORDER BY category, rating
                          LIMIT '{}' OFFSET '{}'""".format(page_display, page_offset)
     else:
-        pagequery = """SELECT * FROM dailyprice
+        pagequery = """SELECT * FROM dailyprice ORDER BY category, rating
                          LIMIT 10 OFFSET '{}'""".format(page_offset)
     dailyprices = db.session.execute(pagequery)
     print(dailyprices, flush=True)
     table = DailyPriceTable(dailyprices)
     return render_template("adminDailyPrice.html", table=table, pagination=pagination, username=current_user.username + " admin")
-=======
-def render_admin_dailyprice():
-    query = "SELECT * FROM dailyprice ORDER BY category, rating"
-    price = db.session.execute(query)
-    print(price, flush=True)
-    table = DailyPriceTable(price)
-    return render_template("adminDailyPrice.html", table=table, username=current_user.username + " admin")
->>>>>>> e8f6741e46ff646c61b8d2b90584acbd6c03ac5a
 
 @view.route("/admin/dailyprice/update", methods=["GET", "POST"])
 @roles_required('admin')
